3790efe491689f7e237155df7b8e76bea3edef44
Aditya <adityafullstackdeveloper@gmail.com>
2022-05-19 18:09:40 +0530

    setup route and layout

---
+++ src/Pages/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <>
+        <h1 className='text-6xl mb-4 text-center'>Welcome to Github Finder App</h1>
+    </>
+  )
+}
+
+export default Index
